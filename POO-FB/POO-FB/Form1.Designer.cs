7934ecbc69152d4ffd90d3ccf768146cebdc5451
Bruno Santos Ichikawa <149208859+BrunoIchikawa@users.noreply.github.com>
2024-05-13 12:19:19 -0300

    FATORIAL

---
+++ POO-FB/POO-FB/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace POO_FB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblInserir = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtFatorial = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(297, 44);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(190, 20);
+            this.txtNum.TabIndex = 0;
+            // 
+            // lblInserir
+            // 
+            this.lblInserir.AutoSize = true;
+            this.lblInserir.Location = new System.Drawing.Point(201, 47);
+            this.lblInserir.Name = "lblInserir";
+            this.lblInserir.Size = new System.Drawing.Size(90, 13);
+            this.lblInserir.TabIndex = 1;
+            this.lblInserir.Text = "Insira um número:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblTitulo.Location = new System.Drawing.Point(338, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(55, 17);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Fatorial";
+            // 
+            // txtFatorial
+            // 
+            this.txtFatorial.Enabled = false;
+            this.txtFatorial.Location = new System.Drawing.Point(297, 93);
+            this.txtFatorial.Multiline = true;
+            this.txtFatorial.Name = "txtFatorial";
+            this.txtFatorial.Size = new System.Drawing.Size(190, 224);
+            this.txtFatorial.TabIndex = 3;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(356, 323);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(216, 363);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.TabIndex = 5;
+            this.btnFor.Text = "FOR";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(297, 363);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnWhile.TabIndex = 6;
+            this.btnWhile.Text = "WHILE";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(412, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(493, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtFatorial);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblInserir);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblInserir;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtFatorial;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ POO-FB/POO-FB/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_FB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtFatorial.Clear();
+            txtNum.Clear();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            double fat, num,i;
+            num = Convert.ToDouble(txtNum.Text);
+            i = num - 1;
+            fat = num * i;
+            txtFatorial.Text = string.Concat(txtFatorial.Text, "\r\n", fat.ToString());
+            i--;
+
+            for ( i = num - 2; i >0 ; i--){
+                fat*=i ;
+                txtFatorial.Text = string.Concat(txtFatorial.Text, "\r\n", fat.ToString());
+                
+            }
+                
+
+            
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            double fat, num, i;
+            num = Convert.ToDouble(txtNum.Text);
+            i = num - 1;
+            fat = num * i;
+            txtFatorial.Text = string.Concat(txtFatorial.Text, "\r\n", fat.ToString());
+            i--;
+
+            while(i>0){
+                i--;
+                fat *= i;
+                txtFatorial.Text = string.Concat(txtFatorial.Text, "\r\n", fat.ToString());
+
+            }
+
+
+        }
+    }
+}
